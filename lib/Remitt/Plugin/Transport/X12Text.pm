ec9261f81faa94909749bd318567aa371e05529e
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2004-06-02 15:57:13 +0000

    Added new dummy X12 text transport.

---
+++ lib/Remitt/Plugin/Transport/X12Text.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+#
+#	$Id$
+#	$Author: jeff $
+#
+#	Dummy X12 plugin to return X12 text
+#
+
+package Remitt::Plugin::Transport::X12Text;
+
+use Data::Dumper;
+
+sub Transport {
+	my ( $input ) = @_;
+
+	return $input;
+} # end method Transport
+
+sub Config {
+	return +{
+		'InputFormat' => 'x12',
+		'OutputFormat' => 'text'
+	};
+} # end sub Config
+
+sub test {
+	my $test = "XYZ";
+	print "Initial string = ";
+	print $test;
+	print "\n---\n";
+	print "Output:\n";
+	print Remitt::Plugin::Transport::X12Text::Transport($test);
+	print "\n---\n";
+} # end sub test
+
+1;
