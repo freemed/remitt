bacc2570a3b78016b1f9cbe50c80023f4ee2a1e6
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2004-06-02 15:56:21 +0000

    Added Translation layer resolution with test script. Added NaturalDocs documentation.

--- lib/Remitt/Utilities.pm
+++ lib/Remitt/Utilities.pm
@@ -42,15 +42,93 @@ sub Authenticate {
 	);
 } # end sub Authenticate
 
+# Function: Fault
+#
+#	Produce "authentication failed" XML-RPC fault.
+#	
 sub Fault {
 	die ("Authentication failed. User not logged in");
 } # end sub Fault
 
+# Function: ForceAuthentication
+#
+#	Force authentication check using basic authentication. Uses
+#	basic authentication username and password to check against
+#	session data, and returns an XML-RPC fault if authentication
+#	fails.
+#
 sub ForceAuthentication {
 	my (undef, $authstring) = split / /, $ENV{'HTTP_authorization'};
 	my ($auth, $sessionid) = Remitt::Utilities::Authenticate($authstring);
 	Remitt::Utilities::Fault() if (!$auth);
 	return 1;
 } # end sub ForceAuthentication
+
+# Function: ResolveTranslationPlugin
+#
+#	The "Translation" layer of plugins for Remitt is a hidden layer,
+#	which means that the user only has to specify a "Render" layer
+#	and "Transport" layer plugin. This function resolves the
+#	plugin that is to be used for the "Translation" layer.
+#
+# Parameters:
+#
+#	$render - Rendering plugin
+#
+#	$renderoption - Rendering plugin option. If this is declared as
+#	'', no render option is needed.
+#
+#	$transport - Transport plugin
+#
+# Returns:
+#
+#	Name of the Translation plugin to be used.
+#
+sub ResolveTranslationPlugin {
+	# Get parameters
+	my ($render, $renderoption, $transport) = @_;
+
+	my $path = '/root/FMSF/remitt';
+
+	# Sanitize parameters
+	$render =~ s/\W//g;
+	$renderoption =~ s/\W//g;
+	$transport =~ s/\W//g;
+
+	# Cache the options for render and transport plugins
+	my $config = { };
+	eval 'use Remitt::Plugin::Render::'.$render.';';
+	eval '$config{"Render"} = Remitt::Plugin::Render::'.$render.'::Config();';
+	eval 'use Remitt::Plugin::Transport::'.$transport.';';
+	eval '$config{"Transport"} = Remitt::Plugin::Transport::'.$transport.'::Config();';
+
+	my $input;
+	# Handle variable output formats with render plugin
+	if ($config{'Render'}->{'OutputFormat'} eq 'variable') {
+		$input = $config{'Render'}->{'Options'}->{$renderoption}->{'OutputFormat'};
+	} else {
+		$input = $config{'Render'}->{'OutputFormat'};
+	}
+	my $output = $config{'Transport'}->{'InputFormat'};
+
+	# Cache the translation layer options
+	my @plugins;
+	opendir DH, $path.'/lib/Remitt/Plugin/Translation/' or die('No translation plugins found.');
+	foreach my $plugin (readdir DH) {
+		if ($plugin =~ /\.pm$/) {
+			my $pluginoptions;
+			$plugin =~ s/\.pm$//;
+
+			# Read information from plugin
+			eval 'use Remitt::Plugin::Translation::'.$plugin.';';
+			eval '$config{"Transport"} = Remitt::Plugin::Translation::'.$plugin.'::Config();';
+			if (($config{'Transport'}->{'InputFormat'} eq $input) and ($config{'Transport'}->{'OutputFormat'} eq $output)) {
+				return $plugin;
+			}
+		}
+	}
+	# If nothing is found, return null
+	return '';
+} # end sub ResolveTranslationPlugin
 
 1;
---
+++ test/test_resolve.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -I./lib
+#
+#	$Id$
+#	$Author: jeff $
+#
+# File: test/test_resolve.pl
+#
+#	Test translation layer resolving.
+#
+
+use Remitt::Utilities;
+
+print "Translation layer resolving\n";
+print "by Jeff Buchbinder\n";
+print "\n";
+
+my $render = shift || 'XSLT';
+my $renderoption = shift || '837p';
+my $transport = shift || 'X12Text';
+
+print "For Render::$render (option $renderoption), Transport::$transport: \n\t";
+print Remitt::Utilities::ResolveTranslationPlugin($render,$renderoption,$transport)."\n";
+
+1;
