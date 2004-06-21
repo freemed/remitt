664d0c82c3835c5b47cbd97e3b08d958067ce52b
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2004-06-21 17:39:02 +0000

    Move out of the way.

--- doc/naturaldocs/src/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# $Id$
-# $Author$
-#
-#	Script to generate naturaldocs documentation. Assumes that
-#	NaturalDocs is installed in /usr/share/naturaldocs.
-#
-
-/usr/share/naturaldocs/NaturalDocs -i . -p doc/naturaldocs/src -o HTML doc/naturaldocs
