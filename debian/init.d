d0c163b3c35c4112cfa69a7b70912a0f07e250b0
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2005-04-15 04:34:24 +0000

    Fix stop problems and obnoxious sysv script process.

--- debian/init.d
+++ debian/init.d
@@ -26,17 +26,12 @@ set -e
 case "$1" in
   start)
 	echo -n "Starting $DESC: "
-	( cd /usr/share/remitt; ./bin/remitt_server.pl 2>&1 > /dev/null ) &
-	#start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
-	#--make-pidfile --background --exec $DAEMON -- $DAEMON_OPTS
+	( cd /usr/share/remitt; ./bin/remitt_server.pl 2>&1 > /dev/null & )
 	echo "$NAME."
 	;;
   stop)
 	echo -n "Stopping $DESC: "
-	# Hacky as hell.. start-stop-daemon doesn't work here
-	kill -9 `cat /var/run/$NAME.pid` 2>&1 > /dev/null
-	#start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
-	#	--exec $DAEMON
+	killall -9 remitt_server.pl 2>&1 > /dev/null
 	echo "$NAME."
 	;;
   #reload)
@@ -58,16 +53,12 @@ case "$1" in
 	#	just the same as "restart".
 	#
 	echo -n "Restarting $DESC: "
-	start-stop-daemon --stop --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON
-	sleep 1
-	start-stop-daemon --start --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS
+	$0 stop
+	$0 start
 	echo "$NAME."
 	;;
   *)
 	N=/etc/init.d/$NAME
-	# echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
 	echo "Usage: $N {start|stop|restart|force-reload}" >&2
 	exit 1
 	;;
