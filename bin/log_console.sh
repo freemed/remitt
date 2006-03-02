e5c77f37ddffa236bd45ac5eb7f4c5e4b24546d2
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2006-03-02 03:51:41 +0000

    Poll for changes in log console every 2 seconds instead of 5.

--- bin/log_console.sh
+++ bin/log_console.sh
@@ -9,4 +9,4 @@ if [ ! -x "$(which sqlite3)" ]; then
 	exit
 fi
 
-watch --interval=5 'sqlite3 spool/log.db "SELECT stamp,username,method,message FROM log ORDER BY OID DESC LIMIT 25"'
+watch --interval=2 'sqlite3 spool/log.db "SELECT stamp,username,method,message FROM log ORDER BY OID DESC LIMIT 25"'
