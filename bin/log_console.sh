d62b8d9c006d2b0d9b9e5e6adad029e8c8a77a82
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2006-04-02 18:45:51 +0000

    Add error message to log console if not in the right place or have no perms.

--- bin/log_console.sh
+++ bin/log_console.sh
@@ -9,4 +9,11 @@ if [ ! -x "$(which sqlite3)" ]; then
 	exit
 fi
 
+if [ ! -r "spool/log.db" ]; then
+	echo "You either don't have permission to read spool/log.db, or you are running"
+	echo "this from a place other than the REMITT root directory (which is usually"
+	echo "/usr/share/remitt) ... "
+	exit
+fi
+
 watch --interval=2 'sqlite3 spool/log.db "SELECT stamp,username,method,message FROM log ORDER BY OID DESC LIMIT 25"'
