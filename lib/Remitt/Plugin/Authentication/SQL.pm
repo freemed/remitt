69b8fa6cba3edbd7720036acb5c34340c7a70ac7
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2007-11-02 01:11:05 +0000

    Move from SQLite specific authentication to DBD-based driver.

--- lib/Remitt/Plugin/Authentication/SQL.pm
+++ lib/Remitt/Plugin/Authentication/SQL.pm
@@ -1,14 +1,33 @@
 #!/usr/bin/perl -w
 #
-#	$Id$
-#	$Author$
+# $Id$
 #
-# Package: Remitt::Plugin::Authentication::SQLite
+# Authors:
+#      Jeff Buchbinder <jeff@freemedsoftware.org>
 #
-#	Authentication plugin using SQLite engine.
+# REMITT Electronic Medical Information Translation and Transmission
+# Copyright (C) 1999-2007 FreeMED Software Foundation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+# Package: Remitt::Plugin::Authentication::SQL
+#
+#	Authentication plugin using SQL (DBD) engine.
 #
 
-package Remitt::Plugin::Authentication::SQLite;
+package Remitt::Plugin::Authentication::SQL;
 
 use FindBin;
 use lib "$FindBin::Bin/../lib";
@@ -17,7 +36,6 @@ use Remitt::Utilities;
 use Data::Dumper;
 use DBI;
 use Digest::MD5 qw(md5_base64);
-require DBD::SQLite;
 
 # Method: Authenticate
 # 
@@ -32,17 +50,16 @@ require DBD::SQLite;
 # Returns:
 #
 # 	Boolean value, depending on whether or not it is matched in the
-# 	SQLite database. This database is located at (home)/spool/auth.db.
+# 	SQL database. This database is located at (home)/spool/auth.db.
 # 	
 sub Authenticate {
 	my ( $user, $pass ) = @_;
 
 	# Make sure database is initialized
-	Remitt::Plugin::Authentication::SQLite::Init();
+	Remitt::Plugin::Authentication::SQL::Init();
 
 	# Open appropriate file
-	my $config = Remitt::Utilities::Configuration ( );
-	my $d = DBI->connect('dbi:SQLite:dbname='.$config->val('installation', 'path').'/spool/auth.db', '', '');
+	my $d = Remitt::Utilities::SqlConnection( );
 	my $s = $d->prepare('SELECT pass FROM auth WHERE user=?');
 	my $r = $s->execute($user);
 	my $data = $s->fetchrow_hashref;
@@ -73,11 +90,10 @@ sub Create {
 	my ( $user, $pass ) = @_;
 
 	# Make sure database is initialized
-	my $_x = Remitt::Plugin::Authentication::SQLite::Init();
+	my $_x = Remitt::Plugin::Authentication::SQL::Init();
 
 	# Open appropriate file
-	my $config = Remitt::Utilities::Configuration ( );
-	my $d = DBI->connect('dbi:SQLite:dbname='.$config->val('installation', 'path').'/spool/auth.db', '', '');
+	my $d = Remitt::Utilities::SqlConnection( );
 	my $s = $d->prepare('INSERT INTO auth ( user, pass ) VALUES ( ?, ? )');
 	my $r = $s->execute($user, md5_base64($pass));
 
@@ -91,7 +107,7 @@ sub Config {
 
 # Method: Delete
 #
-# 	Remove user from SQLite database
+# 	Remove user from SQL database
 #
 # Parameters:
 #
@@ -105,11 +121,10 @@ sub Delete {
 	my ( $user ) = @_;
 
 	# Make sure database is initialized
-	Remitt::Plugin::Authentication::SQLite::Init();
+	Remitt::Plugin::Authentication::SQL::Init();
 
 	# Open appropriate file
-	my $config = Remitt::Utilities::Configuration ( );
-	my $d = DBI->connect('dbi:SQLite:dbname='.$config->val('installation', 'path').'/spool/auth.db', '', '');
+	my $d = Remitt::Utilities::SqlConnection( );
 	my $s = $d->prepare('DELETE FROM auth WHERE user=?');
 	my $r = $s->execute($user);
 
@@ -127,32 +142,23 @@ sub Delete {
 sub Init {
 	# Open appropriate file
 	my $config = Remitt::Utilities::Configuration ( );
-	my $f = $config->val('installation', 'path').'/spool/auth.db';
-	#print "(file = $f) ";
-	if ( -e $f ) {
-		# Skip
-		return 1;
-	} else {
-		my $d = DBI->connect('dbi:SQLite:dbname='.$config->val('installation', 'path').'/spool/auth.db', '', '');
-		my $s = $d->do('CREATE TABLE auth ( user varchar unique, pass varchar)');
-		if ($s) { return 1; } else { return 0; }
-	}
+	return 1;
 } # end method Init
 
 sub test {
 	print " * Creating temporary user ... "; 
-	my $x = Remitt::Plugin::Authentication::SQLite::Create('_test', 'test');
+	my $x = Remitt::Plugin::Authentication::SQL::Create('_test', 'test');
 	if ($x) { print "passed\n"; } else { print "failed\n"; }
 	print " * Checking authentication for user (correct) ... ";
-	$x = Remitt::Plugin::Authentication::SQLite::Authenticate('_test', 'test');
+	$x = Remitt::Plugin::Authentication::SQL::Authenticate('_test', 'test');
 	if ($x) { print "passed\n"; } else { print "failed\n"; }
 	print " * Checking authentication for user (incorrect password) ... ";
-	$x = Remitt::Plugin::Authentication::SQLite::Authenticate('_test', 'test2');
+	$x = Remitt::Plugin::Authentication::SQL::Authenticate('_test', 'test2');
 	if (!$x) { print "passed\n"; } else { print "failed\n"; }
 	print " * Checking authentication for user (incorrect name) ... ";
-	$x = Remitt::Plugin::Authentication::SQLite::Authenticate('_test123', 'test');
+	$x = Remitt::Plugin::Authentication::SQL::Authenticate('_test123', 'test');
 	if (!$x) { print "passed\n"; } else { print "failed\n"; }
-	print " * Removing temporary user ... "; if (Remitt::Plugin::Authentication::SQLite::Delete('_test', 'test')) { print "passed\n"; } else { print "failed\n"; }
+	print " * Removing temporary user ... "; if (Remitt::Plugin::Authentication::SQL::Delete('_test', 'test')) { print "passed\n"; } else { print "failed\n"; }
 } # end sub test
 
 1;
