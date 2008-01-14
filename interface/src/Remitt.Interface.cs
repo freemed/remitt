88a2560d47e8a040c8ddd8fb3fce7e9f00553d61
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2008-01-14 20:58:15 +0000

    Add SubmitJob() and UpdatePassword() methods, fix dependencies.

--- interface/src/Remitt.Interface.cs
+++ interface/src/Remitt.Interface.cs
@@ -27,17 +27,15 @@ using System.Web;
 using System.Web.Services;
 
 using log4net;
+using MySql.Data.MySqlClient;
 
 namespace Remitt
 {
 	[WebService (Description="REMITT public web service", Namespace="http://remitt.org/")]
 	public class Interface : System.Web.Services.WebService
 	{
-		[WebMethod (Description="Adds two numbers")]
-		public int Add (int firstNumber, int secondNumber)
-		{
-			return firstNumber + secondNumber;
-		}
+		// Logging
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType );
 
 		[WebMethod (Description="Retrieve active protocol version")]
 		public string ProtocolVersion ( )
@@ -45,5 +43,96 @@ namespace Remitt
 			return "2.0";
 		}
 
+		[WebMethod (Description="Submit a job for processing by the REMITT engine")]
+		public int SubmitJob( string InputPayload, string RenderPlugin, string RenderOption, string TransportPlugin )
+		{
+			string Username = System.Web.HttpContext.Current.User.Identity.Name;
+			log.Debug( "Submit job for " + Username + " [payload length = " + InputPayload.Length.ToString() + "]" );
+
+			MySqlConnection sql = this.OpenDbConnection( );
+			
+			MySqlCommand cmd = new MySqlCommand( );
+			cmd.Connection = sql;
+			cmd.CommandText = "INSERT INTO jobs ( user, original_data, render_plugin, render_option, transport_plugin ) VALUES ( ?user, ?data, ?renderplugin, ?renderoption, ?transportplugin );";
+			cmd.Prepare();
+			cmd.Parameters.AddWithValue( "?user", Username );
+			cmd.Parameters.AddWithValue( "?data", InputPayload );
+			cmd.Parameters.AddWithValue( "?renderplugin", RenderPlugin );
+			cmd.Parameters.AddWithValue( "?renderoption", RenderOption );
+			cmd.Parameters.AddWithValue( "?transportplugin", TransportPlugin );
+			try {
+				cmd.ExecuteNonQuery( );
+			} catch ( MySqlException ex ) {
+				// In the case of an exception with the insert, return 0
+				log.Error( ex );
+				sql.Close( );
+				return 0;
+			}
+
+			// Get last ID to return
+			int OID = 0;
+			MySqlCommand lastInsertCmd = new MySqlCommand( "SELECT LAST_INSERT_ID();", sql );
+			try {
+				MySqlDataReader reader = lastInsertCmd.ExecuteReader( );
+				reader.Read( );
+				OID = reader.GetInt32( 0 );
+			} catch ( MySqlException ex ) {
+				// In the case of an exception with the insert, return 0
+				log.Error( ex );
+				sql.Close( );
+				return 0;
+			}
+
+			try { sql.Close( ); } catch ( MySqlException ex ) { log.Error( ex ); }
+			return OID;
+		}
+
+		[WebMethod (Description="Change stored password for current user")]
+		public bool UpdatePassword( string Password )
+		{
+			string Username = System.Web.HttpContext.Current.User.Identity.Name;
+			log.Info( "Password change for " + Username );
+
+			MySqlConnection sql = this.OpenDbConnection( );
+			
+			MySqlCommand cmd = new MySqlCommand( );
+			cmd.Connection = sql;
+			cmd.CommandText = "UPDATE auth SET pass = ?pass WHERE user = ?user";
+			cmd.Prepare();
+			cmd.Parameters.AddWithValue( "?user", Username );
+			cmd.Parameters.AddWithValue( "?pass", Password );
+			try {
+				cmd.ExecuteNonQuery( );
+				try { sql.Close( ); } catch ( MySqlException ex1 ) { log.Error( ex1 ); }
+				return true;
+			} catch ( MySqlException ex ) {
+				log.Error( ex );
+				try { sql.Close( ); } catch ( MySqlException ex1 ) { log.Error( ex1 ); }
+				return false;
+			}
+		}
+
+		private MySqlConnection OpenDbConnection ( ) {
+			RemittConfiguration config = new RemittConfiguration( );
+			RemittSettings settings = config.GetSettings( );
+			MySqlConnection conn;
+
+			string connectionString = String.Format(
+				  "server={0};user id={1}; password={2}; database={3}; pooling=false"
+				, settings.DbHost
+				, settings.DbUsername
+				, settings.DbPassword
+				, settings.DbName
+			);
+			try {
+				conn = new MySqlConnection( connectionString );
+				conn.Open( );
+				return conn;
+			} catch ( MySqlException ex ) {
+				log.Error( ex );
+				log.Error( "Error establishing connection : " + connectionString );
+				throw new Exception ( "Error establishing connection to MySQL server" );
+			}
+		}
 	}
 }
