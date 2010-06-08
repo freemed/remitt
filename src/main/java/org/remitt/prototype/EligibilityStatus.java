4ac72e8064cd1d686785488465e95075af8524ca
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2010-06-08 17:32:34 +0000

    Rewrite EligibilityStatus to be an enum, and be much cleaner.

--- src/main/java/org/remitt/prototype/EligibilityStatus.java
+++ src/main/java/org/remitt/prototype/EligibilityStatus.java
@@ -24,100 +24,22 @@
 
 package org.remitt.prototype;
 
-import java.io.ObjectStreamException;
-import java.io.Serializable;
-import java.util.HashMap;
+public enum EligibilityStatus {
+	OK("OK"), BAD("BAD"), CONTINUATION("CONTINUATION"), SERVER_ERROR(
+			"SERVER_ERROR");
 
-import javax.xml.namespace.QName;
+	private final String value;
 
-import WebServices.GatewayEDI.SuccessCode;
-
-public class EligibilityStatus implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	private String _value_;
-	private static HashMap<String, EligibilityStatus> _table_ = new HashMap<String, EligibilityStatus>();
-
-	public EligibilityStatus() {
+	private EligibilityStatus(String value) {
+		this.value = value;
 	}
-
-	// Constructor
-	protected EligibilityStatus(String value) {
-		_value_ = value;
-		_table_.put(_value_, this);
-	}
-
-	public static final String _OK = "OK";
-	public static final String _BAD = "BAD";
-	public static final String _CONTINUATION = "CONTINUATION";
-	public static final String _SERVER_ERROR = "SERVER_ERROR";
-	public static final EligibilityStatus OK = new EligibilityStatus(_OK);
-	public static final EligibilityStatus BAD = new EligibilityStatus(_BAD);
-	public static final EligibilityStatus CONTINUATION = new EligibilityStatus(
-			_CONTINUATION);
-	public static final EligibilityStatus SERVER_ERROR = new EligibilityStatus(
-			_SERVER_ERROR);
 
 	public String getValue() {
-		return _value_;
-	}
-
-	public static EligibilityStatus fromValue(String value)
-			throws IllegalArgumentException {
-		EligibilityStatus enumeration = (EligibilityStatus) _table_.get(value);
-		if (enumeration == null)
-			throw new IllegalArgumentException();
-		return enumeration;
-	}
-
-	public static EligibilityStatus fromString(String value)
-			throws IllegalArgumentException {
-		return fromValue(value);
-	}
-
-	public boolean equals(java.lang.Object obj) {
-		return (obj == this);
-	}
-
-	public int hashCode() {
-		return toString().hashCode();
+		return this.value;
 	}
 
 	public String toString() {
-		return _value_;
-	}
-
-	public Object readResolve() throws ObjectStreamException {
-		return fromValue(_value_);
-	}
-
-	public static org.apache.axis.encoding.Serializer getSerializer(
-			String mechType, Class _javaType, QName _xmlType) {
-		return new org.apache.axis.encoding.ser.EnumSerializer(_javaType,
-				_xmlType);
-	}
-
-	public static org.apache.axis.encoding.Deserializer getDeserializer(
-			String mechType, Class _javaType, QName _xmlType) {
-		return new org.apache.axis.encoding.ser.EnumDeserializer(_javaType,
-				_xmlType);
-	}
-
-	// Type metadata
-	private static org.apache.axis.description.TypeDesc typeDesc = new org.apache.axis.description.TypeDesc(
-			SuccessCode.class);
-
-	static {
-		typeDesc.setXmlType(new javax.xml.namespace.QName(
-				"GatewayEDI.WebServices", "SuccessCode"));
-	}
-
-	/**
-	 * Return type metadata object
-	 */
-	public static org.apache.axis.description.TypeDesc getTypeDesc() {
-		return typeDesc;
+		return this.value;
 	}
 
 }
