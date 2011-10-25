f9f70f54c380ae24fb1c8c340fa69ac146d96387
jeff <jeff@df2b3034-68db-0310-a767-88f7bd87bfb5>
2011-10-25 17:55:42 +0000

    Move to our own servlet rather than using spring for init.

---
+++ src/main/java/org/remitt/server/cxf/RemittRemoteServicesServlet.java
@@ -0,0 +1,28 @@
+package org.remitt.server.cxf;
+
+import javax.servlet.ServletConfig;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.frontend.ServerFactoryBean;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.remitt.server.impl.ServiceImpl;
+
+public class RemittRemoteServicesServlet extends CXFNonSpringServlet {
+
+	private static final long serialVersionUID = 5150445863173665764L;
+
+	@Override
+	public void loadBus(ServletConfig servletConfig) {
+		super.loadBus(servletConfig);
+
+		Bus bus = getBus();
+		BusFactory.setDefaultBus(bus);
+		ServerFactoryBean factory = new ServerFactoryBean();
+		factory.setBus(bus);
+		factory.setServiceClass(ServiceImpl.class);
+		factory.setAddress("/interface");
+		factory.create();
+	}
+
+}
