eb1e4632d253da5873d044646fd7a5bbff837e86
Jeff Buchbinder <jeff@ourexchange.net>
2011-12-30 10:10:27 -0500

    All definitions occur at the top of the source, added copyright notice.

--- src/main/java/org/remitt/plugin/eligibility/NCMedicaidEligibility.java
+++ src/main/java/org/remitt/plugin/eligibility/NCMedicaidEligibility.java
@@ -1,3 +1,27 @@
+/*
+ * $Id$
+ *
+ * Authors:
+ *      Jeff Buchbinder <jeff@freemedsoftware.org>
+ *
+ * REMITT Electronic Medical Information Translation and Transmission
+ * Copyright (C) 1999-2011 FreeMED Software Foundation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package org.remitt.plugin.eligibility;
 
 import org.apache.log4j.Logger;
@@ -12,23 +36,28 @@ public class NCMedicaidEligibility extends SftpEligibility {
 	public static String INSURANCE_ADDRESS_STATE = "NC";
 	public static String INSURANCE_ADDRESS_POSTALCODE = "";
 	public static String INSURANCE_ETIN = "";
+	public static boolean PRODUCTION = true;
+
+	public static String SFTP_HOST = "claims.ncmedicaid.com";
+	public static Integer SFTP_PORT = 22;
+	public static String SFTP_PATH = "In";
 
 	public NCMedicaidEligibility() {
 	}
 
 	@Override
 	public String getSftpHost(String userName) {
-		return "claims.ncmedicaid.com";
+		return SFTP_HOST;
 	}
 
 	@Override
 	public Integer getSftpPort(String userName) {
-		return 22;
+		return SFTP_PORT;
 	}
 
 	@Override
 	public String getSftpPath(String userName) {
-		return "In";
+		return SFTP_PATH;
 	}
 
 	@Override
@@ -63,7 +92,7 @@ public class NCMedicaidEligibility extends SftpEligibility {
 
 	@Override
 	public boolean isProduction() {
-		return true;
+		return PRODUCTION;
 	}
-	
+
 }
